109586fca54c0f84541f38d52838d16874e91251
Z3D-Panthee <Z09876543210d@gmail.com>
2026-09-17 18:43:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SENTINEL OS v9.3 FINAL
-const CACHE='sentinel-v93-hw-v12-v351';
+// SENTINEL OS v9.3 FINAL V3.7
+const CACHE='sentinel-v93-v37-ultra-fluide-final';
 const FILES=['./','./index.html','./manifest.json','/static/icon-192.png','/static/icon-512.png','/static/icon-1024.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>{if(x!==CACHE)return caches.delete(x)}))).then(()=>self.clients.claim()))});
